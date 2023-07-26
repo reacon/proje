e83e619e0ab8e954ba2ac04bb5c1ed4276931a79
Kartik <kartiksindura@Kartik.local>
2023-07-26 23:25:51 +0530

    layouter

---
+++ src/components/Layout.jsx
@@ -0,0 +1,23 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Cool from "../components/Cool";
+import styles from "../styles/Layout.module.css";
+import Last from "../components/Last";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
+
+const Layout = ({children}) => {
+  return (
+    <div className={styles.layout}>
+      <div></div>
+      <Navbar />
+      <div></div>
+      <div></div>
+      <div>
+        {children}
+        <Footer />
+      </div>
+    </div>
+  );
+};
+export default Layout;
