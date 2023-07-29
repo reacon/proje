ed53e27274daf561119f5ef1699a2a38544b72d5
Tanishq Pathak <61777674+reacon@users.noreply.github.com>
2023-07-29 13:30:01 +0530

    feature h bug nhi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,55 @@
 import { Link } from "react-router-dom";
 // eslint-disable-next-line no-unused-vars
-import { useState } from "react";
+import { useRef } from "react";
 
 const Navbar = () => {
-  const [display, setDisplay] = useState("none");
+  const dropdown = useRef();
 
-  const handleClick = () => {
-    if (display == "none") {
-      setDisplay("block");
-    } else {
-      setDisplay("none");
+  const handlenavClick = () => {
+    dropdown.current.classList.toggle("show");
+  };
+
+  const handleOptions = (e) => {
+    if (!dropdown.current.classList.contains(e.target)) {
+      dropdown.current.classList.remove("show");
     }
   };
 
   return (
-    <div className="flex flex-row relative z-0">
-      <Link className="flex-auto border-2 p-7" to="/">
+    <div className="flex flex-row  flex-auto relative z-1">
+      <Link className="  p-7" to="/">
         {" "}
-        WIX
+        <button className="">
+          {" "}
+          <img
+            className="w-12"
+            src="https://img.freepik.com/premium-vector/letter-cc-l…ign-abstract-letter-cc-logo-design_219523-125.jpg"
+            alt=""
+          ></img>
+        </button>
       </Link>
-      <button onClick={handleClick} className="absolute right-0 top-7">
+
+      <button onClick={handlenavClick} className="fixed  right-0 top-10">
         button1
       </button>
 
-      <div
-        className=" absolute z-1 right-5 mr-7 mt-5"
-        style={{ display: display }}
-      >
-        <Link className="m-5">
+      <div className="   dropdown  mt-8 mr-12  " ref={dropdown}>
+        <Link className="m-5" onClick={handleOptions}>
           <button>solutions</button>
         </Link>
-        <Link className="m-5">
+        <Link className="m-5" onClick={handleOptions}>
           <button>vision</button>
         </Link>
-        <Link className="m-5">
+        <Link className="m-5" onClick={handleOptions}>
           <button>programs</button>
         </Link>
-        <Link className="m-5">
+        <Link className="m-5" onClick={handleOptions}>
           <button>blog</button>
         </Link>
-        <Link className="m-5">
+        <Link className="m-5" onClick={handleOptions}>
           <button>log in</button>
         </Link>
-        <Link className="m-5">
+        <Link className="m-5" onClick={handleOptions}>
           <button className="   text-white p-2 rounded-md pl-3 pr-3 transition ease-in-out delay-150 bg-red-600 hover:-translate-y-1 hover:scale-110 hover:bg-red-500 duration-300">
             Get Started
           </button>
